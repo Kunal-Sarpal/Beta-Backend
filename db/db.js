ca47d1e4c4f454528aed15afa3d8a25a9c210b84
Kunal Sarpal <115573463+Kunal-Sarpal@users.noreply.github.com>
2025-03-13 13:20:25 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -38,8 +38,8 @@ const creatorSchema = new mongoose.Schema({
     },
     gstNumber: {
         type: String,
-        required: true,
-        match: [/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}[Z]{1}[0-9A-Z]{1}$/, 'Invalid GST number'] // Updated GST regex
+        // required: true,
+        // match: [/^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}[Z]{1}[0-9A-Z]{1}$/, 'Invalid GST number'] // Updated GST regex
     },
     address: {
         type: String,
